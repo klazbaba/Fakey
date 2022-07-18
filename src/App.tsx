78b432d295dbb96f0dbca9bc8ce6dbeeb6b33fcc
klaz <giwaklaz@gmail.com>
2022-07-18 18:13:06 +0100

    now fetching user contacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Alert,
+  Linking,
+  StyleSheet,
+  View,
+} from "react-native";
+import * as Contacts from "expo-contacts";
 
 export default function App() {
+  const [contacts, setContacts] = useState<Contacts.Contact[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    Contacts.requestPermissionsAsync().then(async ({ status }) => {
+      if (status !== "granted")
+        Alert.alert(
+          "Permission to access contacts is required in order to continue.",
+          "",
+          [
+            { text: "Cancel" },
+            {
+              onPress: () => Linking.openSettings(),
+              text: "Grant Permission",
+            },
+          ]
+        );
+      else if (status === "granted") {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails],
+        });
+        setContacts(data);
+        setIsLoading(false);
+        console.log(JSON.stringify(data, null, 2));
+      }
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ActivityIndicator size={"large"} animating={isLoading} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +49,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
